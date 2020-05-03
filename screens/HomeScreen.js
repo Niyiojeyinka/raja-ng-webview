614c3e1952c93e0c1bb2f08411df010e8463e681
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-05-03 11:43:46 +0100

    removed unused components

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,13 @@
-import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {  Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
-import { MonoText } from '../components/StyledText';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        <Text>Home page here</Text>
+        
 
       </ScrollView>
 

65b30bbb4605b93c87d48cb214922d457e0385ef
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-05-03 18:23:49 +0100

    added webview and changed linkscereen to merchantscreen

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
+import MerchantScreen from '../screens/MerchantScreen';
 
 const BottomTab = createBottomTabNavigator();
 const INITIAL_ROUTE_NAME = 'Home';
@@ -26,7 +26,7 @@ export default function BottomTabNavigator({ navigation, route }) {
       />
       <BottomTab.Screen
         name="Links"
-        component={LinksScreen}
+        component={MerchantScreen}
         options={{
           title: 'Merchant',
           tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="ios-business" />,
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,75 @@
 import * as React from 'react';
-import {  Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, ActivityIndicator, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-import {WebView} from 'react-native-webview';
+import { WebView } from 'react-native-webview';
+const deviceHeight = Dimensions.get('window').height;
+const deviceWidth = Dimensions.get('window').width;
 
 
-export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        
-  <Text>Web Should show here</Text>
+export default class HomeScreen extends React.Component{
 
-      <WebView source={{uri:'https://raja.ng'}} />
+  constructor(props){
+   super(props);
+   this.state={loading:true};
+  }
+
+  showSpinner() {
+    this.setState({ loading: true });
+  }
+  hideSpinner() {
+    this.setState({ loading: false });
+  }
+
+  spinnerView(){
+    return (<ActivityIndicator style={{position: 'absolute',top:0,
+      left:deviceWidth/2}}
+    color="#009688"
+    size="large"/>
+    );
+    
+  }
+
+  errorView(){
+
+    return (
+<view>
+  <Text>Error Occured </Text>
+</view>
+
+    );
+  }
+
+  render(){
+    return(
+
+<View style={styles.container}>
+      <ScrollView style={styles.container}>
+      <WebView style={{flex:1,marginTop:40,width:deviceWidth
+      ,height:deviceHeight*5}}
+  source={{uri: "http://raja.ng"}}
+  onError={(error)=>{ alert("error");}}
+  //onLoadStart={function(){this.}}
+  javaScriptEnabled={true}
+  domStorageEnabled={true}
+  renderLoading={this.spinnerView}
+  renderError={this.errorView}
+  startInLoadingState={true}
+  scalesPageToFit={true}
+/>
       </ScrollView>
 
     </View>
-  );
+    );
+  }
+
 }
+
+
+
+
+
+
+
 
 HomeScreen.navigationOptions = {
   header: null,
@@ -26,9 +79,7 @@ HomeScreen.navigationOptions = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
-  contentContainer: {
-    paddingTop: 10,
-  },
+    backgroundColor: '#fff'
+    
+  }
 });

c4b68a136c0ddb59c9cc80f7d274aacc832f7abe
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-05-06 20:38:33 +0100

    added network error icon

--- screens/ErrorConnectionView.js
+++ screens/ErrorConnectionView.js
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { Ionicons } from '@expo/vector-icons';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 
@@ -7,6 +9,13 @@ export default function ErrorConnectionView(){
     return (
       <View style={styles.container,{justifyContent:"center",alignItems:
         "center" ,flexDirection:"column"}}>
+
+<Ionicons
+      name={props.name}
+      size={100}
+      style={{ margin:16 }}
+      color={"#444444"}
+    />
         <Text style={{marginTop:40,fontSize:32}}>Network Error</Text>
     
         <Text style={{marginTop:20,fontSize:8}}>Will automatically try again in the next few seconds</Text>

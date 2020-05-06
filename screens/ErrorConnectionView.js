4413e703dad8ba45c418fd9c84f564f840157c03
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-05-06 20:18:09 +0100

    added error connection view

---
+++ screens/ErrorConnectionView.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function ErrorConnectionView(){
+
+    return (
+      <View style={styles.container,{justifyContent:"center",alignItems:
+        "center" ,flexDirection:"column"}}>
+        <Text style={{marginTop:40,fontSize:32}}>Network Error</Text>
+    
+        <Text style={{marginTop:20,fontSize:8}}>Will automatically try again in the next few seconds</Text>
+    </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      //backgroundColor: '#fff'
+      
+    }
+  });

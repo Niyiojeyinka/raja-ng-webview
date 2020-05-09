0f618bfdbb7fd7d94fdd93941a131887102a6938
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-05-09 18:42:49 +0100

    added new error network screen

--- screens/ErrorConnectionView.js
+++ screens/ErrorConnectionView.js
@@ -8,15 +8,15 @@ export default function ErrorConnectionView(){
     return (
       <View style={styles.container,{justifyContent:"center",alignItems:
         "center" ,flexDirection:"column"}}>
-        <Text style={{marginTop:40,fontSize:32}}>Network Error</Text>
+     <Text style={{marginTop:40,fontSize:32}}>Network Error</Text>
 
 <Ionicons
-      name={"md-cart"}
-      size={100}
-      style={{ margin:16 }}
+      name={"md-wifi"}
+      size={200}
+      style={{ margin:8}}
       color={"#444444"}
     />
-    
+
         <Text style={{marginTop:20,fontSize:8}}>Will automatically try again in the next few seconds</Text>
     </View>
     );
@@ -26,7 +26,7 @@ export default function ErrorConnectionView(){
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      //backgroundColor: '#fff'
+      backgroundColor: '#fff'
       
     }
   });
